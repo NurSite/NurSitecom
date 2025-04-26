f72dcd125c81dd36bc2af792217636f95659154d
NurSite <Forg5530@gmail.com>
2025-04-27 03:01:48 +0500

    Update render-utils.js

--- src/js/utils/render-utils.js
+++ src/js/utils/render-utils.js
@@ -1,16 +1,19 @@
 /**
- * Постепенный рендеринг
- * @param {Array} data
- * @param {Function} renderFn
- * @param {number} chunkSize
+ * Рендерит массив данных чанками для оптимизации производительности
+ * @param {Array} data - Массив данных
+ * @param {Function} renderCallback - Функция рендеринга каждого элемента
+ * @param {number} chunkSize - Размер чанка (по умолчанию 10)
  */
-export async function renderChunked(data, renderFn, chunkSize = 10) {
+export async function renderChunked(data, renderCallback, chunkSize = 10) {
   for (let i = 0; i < data.length; i += chunkSize) {
+    const chunk = data.slice(i, i + chunkSize);
+    
+    // Используем requestIdleCallback для оптимизации рендеринга
     await new Promise(resolve => {
-      setTimeout(() => {
-        data.slice(i, i + chunkSize).forEach(renderFn);
+      requestIdleCallback(() => {
+        chunk.forEach(item => renderCallback(item));
         resolve();
-      }, 0);
+      });
     });
   }
 }
