528b2e94e93eb511df4a001bde95ca79b79c3e07
NurSite <Forg5530@gmail.com>
2025-04-27 02:18:40 +0500

    Update quran.js

--- src/js/modules/quran.js
+++ src/js/modules/quran.js
@@ -1,53 +1,33 @@
 import { fetchWithCache } from './cache-storage.js';
-import { handleApiError } from '../utils/api.js';
 
 const API_BASE = 'https://api.alquran.cloud/v1';
 const TRANSLATION = 'ru.porokhova';
 
-/**
- * Получает суру с кешированием
- * @param {number} surahNumber (1-114)
- * @returns {Promise<Object>}
- */
 export async function getSurah(surahNumber) {
-  try {
-    const url = `${API_BASE}/surah/${surahNumber}/${TRANSLATION}`;
-    const { data } = await fetchWithCache(url);
-    
-    return {
-      number: data.number,
-      name: data.englishName,
-      arabicName: data.name,
-      ayahs: data.ayahs.map(formatAyah)
-    };
-  } catch (error) {
-    handleApiError(error);
-    throw error;
-  }
-}
-
-/**
- * Получает конкретный аят
- * @param {number} surahNumber 
- * @param {number} ayahNumber 
- * @returns {Promise<Object>}
- */
-export async function getAyah(surahNumber, ayahNumber) {
-  try {
-    const url = `${API_BASE}/ayah/${surahNumber}:${ayahNumber}/${TRANSLATION}`;
-    const { data } = await fetchWithCache(url);
-    return formatAyah(data);
-  } catch (error) {
-    handleApiError(error);
-    throw error;
-  }
-}
-
-function formatAyah(ayah) {
+  const url = `${API_BASE}/surah/${surahNumber}/${TRANSLATION}`;
+  const { data } = await fetchWithCache(url);
+  
   return {
-    number: ayah.numberInSurah,
-    text: ayah.text,
-    translation: ayah.translation || ayah.text,
-    audio: ayah.audio || null
+    number: data.number,
+    name: data.englishName,
+    arabicName: data.name,
+    ayahs: data.ayahs.map(ayah => ({
+      number: ayah.numberInSurah,
+      text: ayah.text,
+      translation: ayah.translation || ayah.text,
+      audio: ayah.audio
+    }))
+  };
+}
+
+export async function getDailyAyat() {
+  const allSurahs = await fetchWithCache(`${API_BASE}/surah`);
+  const randomSurah = allSurahs.data[Math.floor(Math.random() * 114)];
+  const ayahs = await getSurah(randomSurah.number);
+  const randomAyah = ayahs.ayahs[Math.floor(Math.random() * ayahs.ayahs.length)];
+  
+  return {
+    ...randomAyah,
+    surahName: randomSurah.englishName
   };
 }
