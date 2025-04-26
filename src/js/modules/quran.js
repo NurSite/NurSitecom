cfe3b43dff0bbb23dace1422a0cbccda89f3b369
NurSite <Forg5530@gmail.com>
2025-04-27 00:17:15 +0500

    Update quran.js

--- src/js/modules/quran.js
+++ src/js/modules/quran.js
@@ -1,19 +1,53 @@
-import surahs from '../../data/quran.json';
+import { fetchWithCache } from './cache-storage.js';
+import { handleApiError } from '../utils/api.js';
 
-export function getSurahList() {
-  return surahs.map(surah => ({
-    id: surah.number,
-    name: surah.name,
-    ayahs: surah.ayahs.length
-  }));
+const API_BASE = 'https://api.alquran.cloud/v1';
+const TRANSLATION = 'ru.porokhova';
+
+/**
+ * Получает суру с кешированием
+ * @param {number} surahNumber (1-114)
+ * @returns {Promise<Object>}
+ */
+export async function getSurah(surahNumber) {
+  try {
+    const url = `${API_BASE}/surah/${surahNumber}/${TRANSLATION}`;
+    const { data } = await fetchWithCache(url);
+    
+    return {
+      number: data.number,
+      name: data.englishName,
+      arabicName: data.name,
+      ayahs: data.ayahs.map(formatAyah)
+    };
+  } catch (error) {
+    handleApiError(error);
+    throw error;
+  }
 }
 
-export function getDailyAyat() {
-  const randomSurah = surahs[Math.floor(Math.random() * surahs.length)];
-  const randomAyat = randomSurah.ayahs[Math.floor(Math.random() * randomSurah.ayahs.length)];
+/**
+ * Получает конкретный аят
+ * @param {number} surahNumber 
+ * @param {number} ayahNumber 
+ * @returns {Promise<Object>}
+ */
+export async function getAyah(surahNumber, ayahNumber) {
+  try {
+    const url = `${API_BASE}/ayah/${surahNumber}:${ayahNumber}/${TRANSLATION}`;
+    const { data } = await fetchWithCache(url);
+    return formatAyah(data);
+  } catch (error) {
+    handleApiError(error);
+    throw error;
+  }
+}
+
+function formatAyah(ayah) {
   return {
-    surah: randomSurah.name,
-    text: randomAyat.text,
-    number: randomAyat.number
+    number: ayah.numberInSurah,
+    text: ayah.text,
+    translation: ayah.translation || ayah.text,
+    audio: ayah.audio || null
   };
 }
