4b68ef6f3172268334b822c844250d2a0c31bb49
NurSite <Forg5530@gmail.com>
2025-04-28 22:57:23 +0500

    Update auth.js

--- src/utils/helpers/auth.js
+++ src/utils/helpers/auth.js
@@ -1,1 +1,11 @@
+import { Storage } from './storage'
 
+export const Auth = {
+  login(token) {
+    Storage.set('auth_token', token, 86400) // 24 часа
+  },
+
+  logout() {
+    Storage.set('auth_token', null, 0)
+  }
+}
