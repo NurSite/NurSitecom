368bc547c6fd71d134410e247d9c93aea9af62b4
NurSite <Forg5530@gmail.com>
2025-04-28 22:54:49 +0500

    Create router.js

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/quran', component: () => import('@/views/Quran.vue') },
+  { path: '/profile', component: () => import('@/views/Profile.vue') }
+]
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
