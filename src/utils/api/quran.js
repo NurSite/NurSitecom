d53d487fe5ebb92b926c74e3ea8877b91492b7f2
NurSite <Forg5530@gmail.com>
2025-04-28 22:30:18 +0500

    Update quran.js

--- src/utils/api/quran.js
+++ src/utils/api/quran.js
@@ -1,1 +1,22 @@
+import { ref } from 'vue'
 
+const cache = new Map()
+
+export const useQuranAPI = () => {
+  const fetchSurahs = async () => {
+    try {
+      if(cache.has('surahs')) return cache.get('surahs')
+
+      const response = await fetch('https://api.quran.com/api/v4/chapters?language=ru')
+      const data = await response.json()
+      
+      cache.set('surahs', data.chapters)
+      return data.chapters
+    } catch (error) {
+      console.error('Ошибка загрузки сур:', error)
+      return []
+    }
+  }
+
+  return { fetchSurahs }
+}
