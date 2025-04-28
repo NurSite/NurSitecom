cbb4525a7e92bd65fc8d23851358fd9369da2c4e
NurSite <Forg5530@gmail.com>
2025-04-28 22:31:05 +0500

    Update supabase.js

--- src/utils/api/supabase.js
+++ src/utils/api/supabase.js
@@ -1,1 +1,20 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
+
+export const useAuth = () => {
+  const login = async (email, password) => {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+    
+    if(error) throw new Error(error.message)
+    return data
+  }
+
+  return { login }
+}
