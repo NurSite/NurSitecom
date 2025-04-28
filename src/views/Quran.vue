4c29641d049dac1336439554ecc973a687a895b3
NurSite <Forg5530@gmail.com>
2025-04-28 21:24:15 +0500

    Create Quran.vue

---
+++ src/views/Quran.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="quran-page">
+    <AppHeader />
+    <div class="surah-list">
+      <div v-for="surah in surahs" :key="surah.id" class="surah-item">
+        {{ surah.name_arabic }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { fetchSurahs } from '@/utils/api/quran';
+import AppHeader from '@/components/Layout/Header.vue';
+
+export default {
+  components: { AppHeader },
+  data() {
+    return { surahs: [] };
+  },
+  async mounted() {
+    this.surahs = (await fetchSurahs()).chapters;
+  }
+};
+</script>
