668020cfceb41e1f6bd9ba076230816f41eedf62
NurSite <Forg5530@gmail.com>
2025-04-28 22:07:45 +0500

    Update Header.vue

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,1 +1,18 @@
+<template>
+  <header class="header">
+    <svg class="icon">
+      <use href="@/assets/icons/menu.svg#icon"></use>
+    </svg>
+    <h1 class="title">نورسايت</h1>
+  </header>
+</template>
 
+<style scoped>
+.header {
+  font-family: 'Inter', sans-serif;
+}
+
+.title {
+  font-family: 'Amiri Quran', serif;
+}
+</style>
