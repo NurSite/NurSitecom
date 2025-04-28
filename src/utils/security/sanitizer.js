a5634d8da2a165c96953a1861c74c58dbe6abcc5
NurSite <Forg5530@gmail.com>
2025-04-28 23:48:58 +0500

    Update sanitizer.js

--- src/utils/security/sanitizer.js
+++ src/utils/security/sanitizer.js
@@ -4,3 +4,11 @@ export const sanitize = (dirty) => {
     FORBID_ATTR: ['style', 'onerror']
   })
 }
+import DOMPurify from 'dompurify'
+
+export const sanitize = (html) => {
+  return DOMPurify.sanitize(html, {
+    ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'span'],
+    FORBID_ATTR: ['style', 'onclick']
+  })
+}
