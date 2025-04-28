39f208d1fa23e65299ee826dea6559a4f4cc0d2c
NurSite <Forg5530@gmail.com>
2025-04-28 22:35:26 +0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div id="app">
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <style>
-#app {
-  max-width: 1400px;
-  margin: 0 auto;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
