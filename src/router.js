7715a8bd8dcc9bb0c06e3a9e15d4d9f70db78a82
NurSite <Forg5530@gmail.com>
2025-04-29 00:06:56 +0500

    Update router.js

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ const routes = [
   { path: '/', component: Home },
   { path: '/quran', component: () => import('@/views/Quran.vue') },
   { path: '/profile', component: () => import('@/views/Profile.vue') }
+  { path: '/', component: () => import('@/views/Home.vue') }
 ]
 
 export const router = createRouter({
